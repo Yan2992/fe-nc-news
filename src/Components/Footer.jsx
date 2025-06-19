d438f501f2310f0891a3c51ce446b7d30df45778
Yan2992 <yan.bulav@gmail.com>
2025-06-20 00:32:03 +0100

    added favicon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import "../Styles/Footer.css"
 export default function Footer() {
   return (
     <footer>
-      <p>© 2025 Made with ❤️ by Yan Bulabintsev. All rights reserved.</p>
+      <p>© 2025 Made with ❤️ by Yan Bulavintsev. All rights reserved.</p>
     </footer>
   );
 }
